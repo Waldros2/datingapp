5f3c6cf33a21500e2db150cb2dd8a60e5c18f361
Scott Waldron <scottw@ridgeline.solutions>
2021-03-22 14:32:41 -0700

    Added front end changes.

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 
 
@@ -10,13 +11,15 @@ import { ToastrModule } from 'ngx-toastr';
   imports: [
     CommonModule,
     NgbModule,
+    NgxGalleryModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
   ],
   exports: [
     NgbModule,
-    ToastrModule
+    ToastrModule,
+    NgxGalleryModule,
   ]
 })
 export class SharedModule { }
